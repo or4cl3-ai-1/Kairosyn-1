d8178091cf54e933294271b89d9fffcfd10effce
Dustin Groves <declanmacleod228@gmail.com>
2026-04-08 19:14:33 -0700

    init: evaluation subpackage

---
+++ kairosyn/evaluation/__init__.py
@@ -0,0 +1,4 @@
+"""KAIROSYN Evaluation Suite"""
+from kairosyn.evaluation.metrics import KairosynMetrics, compute_ncs_batch, compute_perplexity
+
+__all__ = ["KairosynMetrics", "compute_ncs_batch", "compute_perplexity"]
