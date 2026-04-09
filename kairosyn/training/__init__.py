778a9f222cba6d506de8ba8f67ff17dfb4c6abb6
Dustin Groves <declanmacleod228@gmail.com>
2026-04-08 19:12:08 -0700

    init: training subpackage

---
+++ kairosyn/training/__init__.py
@@ -0,0 +1,4 @@
+"""KAIROSYN Training Pipelines"""
+from kairosyn.training.reward_functions import compute_introspective_reward, RewardWeights
+
+__all__ = ["compute_introspective_reward", "RewardWeights"]
